381aa9da92097aacc545984f1e9489a23ef6d671
NoworNeverAndroid <divergentdev@gmail.com>
2015-08-26 14:15:00 +0200

    Initial commit

---
+++ app/src/main/java/es/com/blogspot/noworneverandroid/watchviewstub/MainActivity.java
@@ -0,0 +1,28 @@
+package es.com.blogspot.noworneverandroid.watchviewstub;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+    private TextView mTextView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView = (TextView) stub.findViewById(R.id.text2);
+            }
+        });
+    }
+}
